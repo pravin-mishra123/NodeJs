7aaab09b42f153f5a286262cd4aabb2b37fc8837
Pravin <pravinmishrag3@gmail.com>
2025-07-16 07:51:12 +0530

    created entry file of node js and cover module system in not js

---
+++ app.js
@@ -0,0 +1,19 @@
+const obj = require("./calculateSum")
+var name = "Namste NodeJs";
+
+var a = 10;
+var b = 20;
+
+// console.log(name);
+// console.log(a +b)
+
+// => global object in node
+
+// console.log(global); // it refere to the global object
+// console.log (this); // it refer to the empty object {}
+// console.log(globalThis); // it refer to the global object in node as well as browser too;
+// console.log(globalThis===global); // both are the same global or globalThis
+
+console.log(obj.calculateSum(a,b))
+console.log(obj.x)
+
---
+++ calculateSum.js
@@ -0,0 +1,7 @@
+var x = "Hello Pravin Mishra!!";
+function calculateSum (a,b){
+    const result = a + b;
+    console.log(result);
+}
+
+module.exports = {x,calculateSum};
