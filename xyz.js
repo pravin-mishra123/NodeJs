16d02ac579ecf9d87cd6172a2ce4865018c50618
Pravin <pravinmishrag3@gmail.com>
2025-07-19 21:50:08 +0530

    updated the code

---
+++ xyz.js
@@ -0,0 +1,13 @@
+// const util = require("node:util")
+// const data = require("./data.json")
+
+// require("./path")
+
+// All the code of the module is wraped inside a function (IIFE);
+
+// IIFE - Immediate Invoked Function Expression
+
+// (function (module, require) {
+//   All the code of the module
+// })();
+
